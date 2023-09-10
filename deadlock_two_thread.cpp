99a0c3909c000f0567b0198a4efe3405df6a2e13
IgorAmashukeli <amashukeliim1150@gmail.com>
2023-09-10 21:42:22 +0100

    deadlock

---
+++ deadlock_two_thread.cpp
@@ -0,0 +1,64 @@
+#include <mutex>
+#include <wheels/test/framework.hpp>
+
+#include "../barrier.hpp"
+
+// https://gitlab.com/Lipovsky/tinyfibers
+#include <tf/sched/spawn.hpp>
+#include <tf/sched/yield.hpp>
+#include <tf/sync/mutex.hpp>
+#include <tf/sync/wait_group.hpp>
+
+// idea: we create two mutexes, each one does a lock
+// however we do lock order inversion (we don't follow resource ordering)
+// therefore we get a deadlock
+// yeild is used to do the switching through the core
+
+using tf::Mutex;
+using tf::Spawn;
+using tf::WaitGroup;
+using tf::Yield;
+
+void TwoFibersDeadLock() {
+  // Mutexes
+  Mutex a;
+  Mutex b;
+
+  // Fibers
+
+  auto first = [&] {
+    // I am a Fiber
+    std::lock_guard guard_a(a);
+    Yield();
+    std::lock_guard guard_b(b);
+  };
+
+  auto second = [&] {
+    // I am a Fiber
+    std::lock_guard guard_b(b);
+    Yield();
+    std::lock_guard guard_a(a);
+  };
+
+  // No deadlock with one fiber
+
+  // No deadlock expected here
+  // Run routine twice to check that
+  // routine leaves mutexes in unlocked state
+
+  Spawn(first).Join();
+  Spawn(first).Join();
+
+  // Same for `second`
+  Spawn(second).Join();
+  Spawn(second).Join();
+
+  ReadyToDeadLock();
+
+  // Deadlock with two fibers
+  WaitGroup wg;
+  wg.Spawn(first).Spawn(second).Wait();
+
+  // We do not expect to reach this line
+  FAIL_TEST("No deadlock =(");
+}
